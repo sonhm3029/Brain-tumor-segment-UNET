f1aaba327119c8606d5389f753029f60bfde0740
sonhm2329 <hoangminhson29102001@gmail.com>
2023-07-23 17:30:02 +0700

    Thêm utils trainign + validation + checkpoint

--- dataset.py
+++ dataset.py
@@ -1,10 +1,45 @@
 from torch.utils.data import Dataset
+from torchvision.transforms import ToTensor
+from typing import Literal
+
+import os
+from PIL import Image
+
 
 
 class BrainTumorDataset(Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, root, part: Literal["train", "test", "valid"], transforms=None):
+        self.root = root
+        self.data_dir = f"{root}/{part}"
+        self.transforms = transforms
+           
+        self.images = [
+            f"{self.data_dir}/{img}" for img in os.listdir(self.data_dir)
+                       if "_mask" not in img and not img.endswith(".csv")
+            ]
+        self.targets = [
+            f"{self.data_dir}/{img.replace('.jpg','')}_mask.png" for img in os.listdir(self.data_dir)
+                       if "_mask" not in img and not img.endswith(".csv")
+            ]
+
     def __len__(self):
-        pass
+        return len(self.images)
+
     def __getitem__(self, index):
-        pass
+        img, label = self.images[index], self.targets[index]
+        print(img)
+        print(label)
+        img = Image.open(img)
+        label = Image.open(label)
+        if self.transforms:
+            img = self.transforms(img)
+            label = self.transforms(label)
+            
+        return img, label
+
+
+if __name__ == "__main__":
+    dataset = BrainTumorDataset(root="dataset", part="train", transforms=ToTensor())
+    
+    img, label = dataset[0]
+    print(img.size(), label.size())
--- utils.py
+++ utils.py
@@ -1,5 +1,78 @@
-def save_checkpoint():
-    pass
 
-def load_checkpoint():
-    pass
+from tqdm import tqdm
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+import os
+
+writer = SummaryWriter("runs")
+
+def save_checkpoint(state, epoch, val_acc, dice_scrore, save_folder="runs/checkpoint"):
+    
+    print(f"""=> Saving checkpoint at epoch {epoch}:
+                \n Validation accuracy: {val_acc}
+                \n Validation dice score: {dice_scrore}
+          """)
+    
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
+    torch.save(state, f"{save_folder}/model_epoch_{epoch}.pth.tar")
+
+def load_checkpoint(checkpoint, model):
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint["state_dict"])
+
+def train_epoch(net, epoch, dataloader,
+                device, optimizer, criterion):
+    net.train() # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(dataloader, 0):
+        images, labels = data[0].to(device), data[1].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 10 == 9:  # print every 100 mini-batches
+            writer.add_scalar('Training loss',
+                                running_loss / 10,
+                                epoch * len(dataloader) + i
+                                )
+            print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 10))
+            running_loss = 0.0
+
+def val_epoch(net, epoch, dataloader, device):
+    net.eval()
+    num_correct = 0
+    num_pixels = 0
+    dice_score = 0
+    with torch.no_grad():
+        for i, data in enumerate(dataloader, 0):
+            images, labels = data[0].to(device), data[1].to(device)
+            
+            preds = torch.sigmoid(net(images))
+            preds = (preds > 0.5).float()
+            
+            num_correct += (preds == labels).sum()
+            num_pixels += torch.numel(preds)
+            dice_score += (2* (preds * labels).sum())/((preds + labels).sum() + 1e-8)
+            
+    val_acc = num_correct*100/num_pixels
+    dice_score = dice_score / len(dataloader)
+    print(f"Valdation result: {num_correct}/{num_pixels} with acc {val_acc:.2f}")
+    print(f"Dice score: {dice_score/len(dataloader)}")
+    writer.add_scalar("Validation Accuracy",
+                      val_acc,
+                      epoch)
+    writer.add_scalar("Dice score",
+                      dice_score,
+                      epoch)
+            
+            
